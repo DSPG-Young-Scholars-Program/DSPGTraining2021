07ee5df50b7f7c9f803481b846f93835fb640532
dads2busy <dads2busy@gmail.com>
2021-06-15 15:13:36 -0400

    db query example update

--- modules/06_intro_to_r/05_relational_data_and_sql/sdad_postgis_login_example.R
+++ modules/06_intro_to_r/05_relational_data_and_sql/sdad_postgis_login_example.R
@@ -1,6 +1,7 @@
 library(RPostgreSQL)
 library(sf)
 
+# Create a Database Connection - you will normally not want to store your credentials here!
 con <- dbConnect(
   drv = PostgreSQL(),
   dbname = "sdad",
@@ -10,13 +11,30 @@ con <- dbConnect(
   password = "exampleexample"
 )
 
+# Use connection to query database. In this case we us sf::st_read because the table has a geography/geometry column we want to use.
 arlington_county_tracts <- st_read(dsn = con,
                                    query = "SELECT *
                                             FROM gis_census_cb.cb_2018_51_tract_500k
                                             WHERE \"COUNTYFP\" = '013'")
 
+# Plot just the geometry
+plot(st_geometry(arlington_county_tracts))
+# Plot one of the columns with the geometry
+plot(arlington_county_tracts[, c("ALAND")])
+
+# Writing tables.
+# Everyone can write tables to the public schema. If you don't include a schema, it will assume "public".
+
+# load some data
+data("mtcars")
+
+# write data.frame to the database
+dbWriteTable(con, "example_mt_cars", mtcars, row.names = T)
+
+# query to make sure it's there (here we use dbGetQuery instead of st_read, there is no geometry in this table)
+dbGetQuery(con, "select * from public.example_mt_cars")
+
+# Be a good citizen and delete your connections when finished
 dbDisconnect(con)
 
-plot(st_geometry(arlington_county_tracts))
 
-plot(arlington_county_tracts[, c("ALAND")])
